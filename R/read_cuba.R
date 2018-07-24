25ebc5f10d2898e12e5f30649bac3461ab2f911b
Alexander Keth <alexander.keth@posteo.net>
2018-07-24 23:26:01 +0200

    Dev Version read_cuba.

---
+++ R/read_cuba.R
@@ -0,0 +1,12 @@
+#' Read in *.grib2 data
+#' 
+#' @param grib2 Chr Canonical Filename
+#' @return Tibble
+#' @export
+#' 
+#' @examples
+
+# https://stackoverflow.com/questions/7622090/how-to-read-grib-files-with-r-gdal
+read_cuba <- function(grib2) {
+  
+}
