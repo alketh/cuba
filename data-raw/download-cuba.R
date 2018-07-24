54b58c17bd418ea081f332c05f54877e53974e72
Alexander Keth <alexander.keth@posteo.net>
2018-07-24 23:24:01 +0200

    Download in case file does not exist.

--- data-raw/download-cuba.R
+++ data-raw/download-cuba.R
@@ -2,5 +2,5 @@ fn <- get_cuba()
 dfn <- file.path("inst", "extdata", basename(fn))
 
 for (i in seq_along(fn)) {
-  download.file(fn[i], dfn[i])
+  if (!file.exists(dfn[i])) download.file(fn[i], dfn[i])
 }
