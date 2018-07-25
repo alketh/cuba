27cd318472d649f352272ac7f9aa2d12b35a4d45
Alexander Keth <alexander.keth@posteo.net>
2018-07-25 21:41:19 +0200

    Error in .local(.Object, ...) :

--- R/read_cuba.R
+++ R/read_cuba.R
@@ -1,12 +1,23 @@
 #' Read in *.grib2 data
-#' 
+#'
 #' @param grib2 Chr Canonical Filename
 #' @return Tibble
 #' @export
-#' 
+#'
 #' @examples
+path <- file.path("inst", "extdata")
+files <- list.files(path, full.names = TRUE, pattern = "\\.grb2")
+grib2 <- files[1]
 
 # https://stackoverflow.com/questions/7622090/how-to-read-grib-files-with-r-gdal
 read_cuba <- function(grib2) {
-  
+
 }
+
+gribfile <- grib2
+library(rgdal)
+library(sp)
+gribfile<-"wavedata.grib"
+grib <- readGDAL(gribfile)
+
+summary = GDALinfo(gribfile,silent=TRUE)
